5187986ec19376926cc1e7becd38bee2d1695f05
hanni01 <he3080@gmail.com>
2023-01-28 22:37:53 +0900

    arrange some files

---
+++ RPGGame/Assets/01_Script/EnemyMove.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    public float speed;
+    public float radius;
+    public float distance;
+    public LayerMask isLayer;
+    public LayerMask isGroundLayer;
+    float minDepth;
+    public Rigidbody2D rb;
+    public float jump_speed;
+    public GameObject player;
+    private bool isground;
+    private bool isend;
+    Animator aniEnemy;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        aniEnemy = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        aniEnemy.SetBool("isRun", false);
+    }
+
+    void FixedUpdate()
+    {
+        Collider2D raycastCollider = Physics2D.OverlapCircle(transform.position, radius, isLayer, minDepth);
+        RaycastHit2D raycast = Physics2D.Raycast(transform.position, transform.right * -1, distance, isGroundLayer);
+        if(!aniEnemy.GetBool("isCollided") == true)
+        {
+            if(raycastCollider != null)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, raycastCollider.transform.position, Time.deltaTime * speed);
+                if(transform.position.x < player.GetComponent<Transform>().position.x)
+                {
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                }
+                aniEnemy.SetBool("isRun", true);
+            }
+            else
+            {
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
+                if(isend)
+                {
+                    if(transform.rotation.eulerAngles.y == 180)
+                    {
+                        transform.eulerAngles = new Vector3(0, 0, 0);
+                        isend = false;
+                    }
+                    else
+                    {
+                        transform.eulerAngles = new Vector3(0, 180, 0);
+                        isend = false;
+                    }
+                }
+                aniEnemy.SetBool("isRun", true);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "grass")
+        {
+            isground = true;
+        }
+        else
+        {
+            isground = false;
+        }
+        if(other.gameObject.tag == "endPoint")
+        {
+            isend = true;
+            Debug.Log("end!!");
+        }
+    }
+}
---
+++ RPGGame/Assets/01_Script/charRespawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charRespawn : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject[] respawnP = new GameObject[4];
+    GameObject nearRespawn;
+    float dist;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.name == "floorEnd")
+        {
+            transform.position = CalculateDist().transform.position;
+        }
+    }
+
+    GameObject CalculateDist()
+    {
+        nearRespawn = respawnP[0];
+        dist = Vector2.Distance(Player.transform.position, nearRespawn.transform.position);
+        for(int i = 1;i < respawnP.Length;i++)
+        {
+            if(dist > Vector2.Distance(Player.transform.position, respawnP[i].transform.position))
+            {
+                nearRespawn = respawnP[i];
+            }
+        }
+        return nearRespawn;
+    }
+}
